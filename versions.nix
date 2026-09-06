ab07a2835debfc2875200d6a3b7d65ef909de0eb
gnkz <gnkz.dev@gmail.com>
2026-09-06 09:14:53 -0300

    chore: update Helium to 0.16.5.1

--- versions.nix
+++ versions.nix
@@ -1,4 +1,4 @@
 {
-  version = "0.16.2.1";
-  hash = "sha256-gAg4BpJyhwpvT8nq3wF8CBn32Jq/YHEXCAsHnUv3wBc=";
+  version = "0.16.5.1";
+  hash = "sha256-9hWnc1ZjWENkCGor6T8OeboSOKhWvm3bta73PnyUqXA=";
 }
